ad45aecaebd3648ed16091f99c15d824599e2934
Yatharth Dubey <yatharthdubey12dec2004@gmail.com>
2025-09-16 23:30:24 +0530

    6th commit

--- src/index.js
+++ src/index.js
@@ -2,25 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import CompleteForm from './CompleteForm';
 import reportWebVitals from './reportWebVitals';
-import StudentReg from './School_Fees/StudentReg';
-import Admin from './School_Fees/admin';
-// import Registration from './Registration';
-// import Dashboard from './Dashboard';
-// import { Login } from './Login';
-// import Stegano from './Steganography/Stegano';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <StudentReg/> */}
-    {/* <Admin/> */}
     <App />
-    {/* <Dashboard/> */}
-    {/* <Login/> */}
-    {/* <Registration/> */}
-    {/* <Stegano/> */}
   </React.StrictMode>
 );
 
